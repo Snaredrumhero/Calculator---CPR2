2c20ee25701fcd43ca1f359fcfcaf6ba799e8ddf
Drew Mullett <dmullett02@gmail.com>
2024-09-10 15:18:59 -0400

    Added preparations for LCD I2C

--- calculator.ino
+++ calculator.ino
@@ -1,24 +1,34 @@
 #include <Arduino.h>
 #include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
 // put function declarations here:
 int myFunction(int, int);
 const int ROWS = 4;
 const int COLS = 4;
+const int rowPins[ROWS] = {};
+const int colPins[COLS] = {};
+
 char keys[ROWS][COLS] = {
 {'1', '2', '3', 'A'},
 {'4', '5', '6', 'B'},
 {'7', '8', '9', 'C'},
 {'*', '0', '#', 'D'}
 };
-
 Keypad keypad = Keypad(makeKeymap(keys), , , ROWS, COLS);
+
+const int ADDRESS = 0x20;
+LiquidCrystal lcd = LiquidCrystal_I2C(ADDRESS, 16, 2);
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  Wire.begin();
   Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
 }
 
 void loop() {
